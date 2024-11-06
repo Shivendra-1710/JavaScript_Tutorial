2c63fcbcdbaeb5225f533d793538e4bc3877992b
Shivendra-1710 <abhi83514@gmail.com>
2024-11-06 20:04:45 +0000

    Config

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all Js code as newer version
+
+// alert (3 + 3) // we are using node js, not browser
+
+console.log(3 
+    + 3);     // code readablility must be high, no matter run correctly or not. //gives 6
+
+
+//Data_Types
+
+let name = "Abhi"
+let age = 20
+let isloggedin = true
+
+/*
+number => 2 to power 53
+bigint 
+string =>  ""
+boolean => true/false
+null => standalone value
+undefined =>
+symbol => uniqueness // mostly used in React      
+*/
+
+//object
+
+console.log(typeof "Abhi");        //String
+
+console.log(typeof null);        // object     // consider as an language error many time 
+
+console.log(typeof undefined);     //undefined
+
+
